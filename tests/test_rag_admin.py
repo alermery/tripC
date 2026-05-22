ff79dbcd0d9b72cce3936845cc6ade660241b62e
alermery <166374478+alermery@users.noreply.github.com>
2026-05-22 15:55:01 +0800

    Delete tests directory

--- tests/test_rag_admin.py
+++
@@ -1,6 +0,0 @@
-from backend.app.api import rag_admin
-
-
-def test_rag_task_registry_shape():
-    assert isinstance(rag_admin._tasks, dict)
-    assert rag_admin._ALLOWED == {".txt", ".csv", ".xlsx"}
